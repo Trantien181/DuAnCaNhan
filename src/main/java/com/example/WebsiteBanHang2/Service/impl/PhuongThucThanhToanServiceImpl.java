1d1107bceb9ad68d35dc28c14ac1b733e352d62c
Trantien181 <tranmanhtien181@gmail.com>
2025-02-26 21:45:47 +0700

    pushtest

--- src/main/java/com/example/WebsiteBanHang2/Service/impl/PhuongThucThanhToanServiceImpl.java
+++ src/main/java/com/example/WebsiteBanHang2/Service/impl/PhuongThucThanhToanServiceImpl.java
@@ -1,4 +1,34 @@
 package com.example.WebsiteBanHang2.Service.impl;
 
-public class PhuongThucThanhToanServiceImpl {
+import com.example.WebsiteBanHang2.Dto.PhuongThucThanhToanDTO;
+import com.example.WebsiteBanHang2.Repository.PhuongThuocThanhToanRepository;
+import com.example.WebsiteBanHang2.Service.PhuongThucThanhToanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class PhuongThucThanhToanServiceImpl implements PhuongThucThanhToanService {
+    @Autowired
+    PhuongThuocThanhToanRepository phuongThuocThanhToanRepository;
+
+    @Override
+    public List<PhuongThucThanhToanDTO> getList() {
+        return List.of();
+    }
+
+    @Override
+    public PhuongThucThanhToanDTO getPhuongThucThanhToanById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public PhuongThucThanhToanDTO createEndUpdatePhuongThucThanhToan(PhuongThucThanhToanDTO phuongThucThanhToanDTO) {
+        return null;
+    }
+
+    @Override
+    public void deletePhuongThucThanhToan(Integer id) {
+
+    }
 }
--- src/main/java/com/example/WebsiteBanHang2/Service/impl/SanPhamChiTietServiceImpl.java
+++ src/main/java/com/example/WebsiteBanHang2/Service/impl/SanPhamChiTietServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.WebsiteBanHang2.Service.impl;
 
 import com.example.WebsiteBanHang2.Dto.SanPhamChiTietDTO;
+import com.example.WebsiteBanHang2.Model.SanPhamChiTiet;
 import com.example.WebsiteBanHang2.Repository.SanPhamChiTietRepository;
 import com.example.WebsiteBanHang2.Service.SanPhamChiTietService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,5 +10,32 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class SanPhamChiTietServiceImpl {
+public class SanPhamChiTietServiceImpl implements SanPhamChiTietService {
+    @Autowired
+    SanPhamChiTietRepository sanPhamChiTietRepository;
+
+    @Override
+    public List<SanPhamChiTietDTO> getList() {
+        return sanPhamChiTietRepository.findAll();
+    }
+
+    @Override
+    public SanPhamChiTietDTO getSanPhamChiTietById(Integer id) {
+        return sanPhamChiTietRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public SanPhamChiTietDTO createEndUpdateSanPhamChiTiet(SanPhamChiTietDTO sanPhamChiTietDTO) {
+        return sanPhamChiTietRepository.save(sanPhamChiTietDTO);
+    }
+
+    @Override
+    public void deleteSanPhamChiTiet(Integer id) {
+        SanPhamChiTietDTO sanPhamChiTietDTO = sanPhamChiTietRepository.findById(id).orElse(null);
+        if (sanPhamChiTietDTO != null) {
+            sanPhamChiTietRepository.deleteById(id);
+        }else {
+
+        }
+    }
 }
